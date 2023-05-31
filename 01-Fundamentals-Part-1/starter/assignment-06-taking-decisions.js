6543fe040e4fa25e36ea0853fa80ec605dbbc78f
chris00walker <chris00walker@gmail.com>
2023-05-30 20:53:59 -0400

    Use if/else statements to log difference between country populations

---
+++ 01-Fundamentals-Part-1/starter/assignment-06-taking-decisions.js
@@ -0,0 +1,34 @@
+
+const barbadosPopulation = 300000;
+let average;
+if (barbadosPopulation > 33000000) {
+    average = `Barbados' population is above average`
+} else {
+    const belowAverage = 33000000 - barbadosPopulation;
+    average = `Barbados' population is ${belowAverage} below average`
+}
+console.log(average);
+
+/*
+const barbadosPopulation = 13000;
+let average;
+if (barbadosPopulation > 33000000) {
+    average = `Barbados' population is above average`
+} else {
+    const belowAverage = 33000000 - barbadosPopulation;
+    average = `Barbados' population is ${belowAverage} below average`
+}
+console.log(average);
+*/
+
+/*
+const barbadosPopulation = 130000000;
+let average;
+if (barbadosPopulation > 33000000) {
+    average = `Barbados' population is above average`
+} else {
+    const belowAverage = 33000000 - barbadosPopulation;
+    average = `Barbados' population is ${belowAverage} below average`
+}
+console.log(average);
+*/
