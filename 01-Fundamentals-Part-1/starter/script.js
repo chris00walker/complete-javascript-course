0ab755bf4eb1308f6a719b1d6e74d390f508745b
chris00walker <chris00walker@gmail.com>
2023-05-31 13:42:35 -0400

    [Complete code along for type conversion and coercion]

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -111,6 +111,7 @@ multiple
 lines`);
 */
 
+/*
 const age = 19;
 
 if (age >= 18) {
@@ -129,3 +130,27 @@ if (birthYear <= 2000) {
     century = 21;
 }
 console.log(century);
+*/
+
+/*
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - '3');
+console.log('23' * '2');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+*/
+
+
