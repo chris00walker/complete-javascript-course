63a4b4d882b2710c3d6c76aee88b142e5b5d3796
chris00walker <chris00walker@gmail.com>
2023-05-31 16:27:18 -0400

    [Complete assignment based of number of Neighboring countries to mine]

---
+++ 01-Fundamentals-Part-1/starter/assignment-08-equality-operators.js
@@ -0,0 +1,35 @@
+//step 1 - 5
+/*
+const numNeighbors = (prompt('How many neighbor countries does your country have?'));
+
+if (numNeighbors == 1) {
+    console.log('Only 1 border');
+} else if (numNeighbors > 1) {
+    console.log('More than 1 border');
+} else {
+    console.log('No borders');
+}
+*/
+//step 6
+/*
+if (numNeighbors === 1) { //when there is exactly 1 border the precisely equal to does not automatically coerce from string to number. So it would default to no borders.
+    console.log('Only 1 border');
+} else if (numNeighbors > 1) {
+    console.log('More than 1 border');
+} else {
+    console.log('No borders');
+}
+*/
+//Step 7 - 8 
+const numNeighbors = Number(prompt('How many neighbor countries does your country have?'));
+if (numNeighbors === 1) { //This produces the desired result 'Only 1 border' - the string value has been converted to a number value
+    console.log('Only 1 border');
+} else if (numNeighbors > 1) {
+    console.log('More than 1 border');
+} else {
+    console.log('No borders');
+}
+
+// using the precisely equal to and number conversion in this situation makes sense as
+// both data values are intended to be numerical. Since the string value is useful the
+// initial data input it must be converted using the Number function for precision.
